1200c5e23316aa1a618293f59a031b4de405da5e
Rub_Rub <D_I123@mail.ru>
2024-10-07 17:34:32 +0300

    added database auto-generation

--- KanbanDesk_Task1/App.cs
+++ KanbanDesk_Task1/App.cs
@@ -1,18 +1,29 @@
-﻿using KanbanDesk_Task1;
+﻿using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace KanbanDesk_Task1;
 
 public class App
 {
+    private const string FileName = "DataBase.json";
+    private static readonly string _saveFilePath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + FileName;
+    
+    private static void CreateAdmin()
+    { 
+        User admin = new User(true, "admin", "123");
+        
+        List<User> users = [admin];
+        
+        var json = JsonConvert.SerializeObject(users);
+        
+        File.WriteAllText(_saveFilePath, json);
+        
+        Console.WriteLine("First program startup –> admin user was created.\nLogin: admin\nPassword: 123");
+    }
+    
     private static void Main(string[] args)
     {
-        string _saveFilePath = Environment.CurrentDirectory;
-        string _fileName = "DataBase.json";
-
-        if (!File.Exists(_saveFilePath))
-        {
-            File.Create(_saveFilePath).Dispose();
-        }
-        
         var services = new ServiceCollection()
             .AddTransient<IDBManager, DataBaseControl>()
             .AddTransient<Sign>()
@@ -20,6 +31,12 @@ public class App
         
         var serviceProvider = services.BuildServiceProvider();
         Kanban kanban = new Kanban(serviceProvider.GetService<IDBManager>(), serviceProvider.GetService<Sign>());
+        
+        if (!File.Exists(_saveFilePath))
+        {
+            File.Create(_saveFilePath).Dispose();
+            CreateAdmin();
+        }
         
         User user = serviceProvider.GetService<Sign>().SignProccess();
         serviceProvider.GetService<Kanban>().Commands(user);
